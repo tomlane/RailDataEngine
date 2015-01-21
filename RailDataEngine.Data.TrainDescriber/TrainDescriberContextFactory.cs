43f0055dc93e7cd035bd33a612e29961b25625ab
Tom Lane <tom@tomlane.me>
2015-01-21 18:50:14 +0000

    Create missing train describer context factory

---
+++ RailDataEngine.Data.TrainDescriber/TrainDescriberContextFactory.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity.Infrastructure;
+using RailDataEngine.Data.Common;
+
+namespace RailDataEngine.Data.TrainDescriber
+{
+    public class TrainDescriberContextFactory : IDbContextFactory<TrainDescriberContext>
+    {
+        public TrainDescriberContext Create()
+        {
+            ITrainDescriberDatabase db = new TrainDescriberDatabase(new ConfigConnectionStringProvider());
+            return db.BuildContext() as TrainDescriberContext;
+        }
+    }
+}
