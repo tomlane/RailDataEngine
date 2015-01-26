d3fb3b76838a7dc8c9f13400f3d16824ff5b916b
Tom Lane <tom@tomlane.me>
2015-01-26 21:41:26 +0000

    Configuring unity on api project

---
+++ RailDataEngine.Api/App_Start/UnityConfig.cs
@@ -0,0 +1,29 @@
+using System;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using RailDataEngine.DI;
+
+namespace RailDataEngine.Api.App_Start
+{
+    /// <summary>
+    /// Specifies the Unity configuration for the main container.
+    /// </summary>
+    public class UnityConfig
+    {
+        #region Unity Container
+        private static Lazy<IUnityContainer> container = new Lazy<IUnityContainer>(() =>
+        {
+            var container = ContainerBuilder.Build();
+            return container;
+        });
+
+        /// <summary>
+        /// Gets the configured Unity container.
+        /// </summary>
+        public static IUnityContainer GetConfiguredContainer()
+        {
+            return container.Value;
+        }
+        #endregion
+    }
+}
