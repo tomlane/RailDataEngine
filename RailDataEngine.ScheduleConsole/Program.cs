c4d97d1a15ce58e05ddb62303fef9a898305e63c
Tom Lane <tom@tomlane.me>
2015-02-11 17:54:47 +0000

    Modify schedule console

--- RailDataEngine.ScheduleConsole/Program.cs
+++ RailDataEngine.ScheduleConsole/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.Practices.Unity;
 using RailDataEngine.DI;
 using RailDataEngine.Domain.Boundary.Schedule.SaveScheduleMessageBoundary;
@@ -10,31 +10,36 @@ namespace RailDataEngine.ScheduleConsole
 {
     class Program
     {
+        private static IUnityContainer _container;
+        private static int _counter;
+
         static void Main(string[] args)
         {
+            _container = ContainerBuilder.Build(); 
+
             HibernatingRhinos.Profiler.Appender.EntityFramework.EntityFrameworkProfiler.Initialize();
-
-            const int lineCount = 10000;
-
-            var container = ContainerBuilder.Build();
             
             var reader = new StreamReader("schedule.json");
-            List<string> lines;
-            int counter = 0;
-            
-            while ((lines = reader.ReadLines(lineCount)).Count > 1)
-            {
-                var boundary = container.Resolve<ISaveScheduleMessagesBoundary>();
-                var request = new SaveScheduleBoundaryRequest
-                {
-                    MessagesToSave = lines
-                };
-                counter += lines.Count;
-                boundary.Invoke(request);
-                Console.WriteLine("{0} lines imported.", counter);
-            }
+            List<string> lines = reader.ReadLines(1000000);
+
+            Parallel.ForEach(lines, SaveMessage);
 
             Console.WriteLine("Schedule imported successfully.");
+        }
+
+        private static void SaveMessage(string message)
+        {
+            _counter++;
+            var boundary = _container.Resolve<ISaveScheduleMessagesBoundary>();
+            var request = new SaveScheduleBoundaryRequest
+            {
+                MessagesToSave = new List<string>
+                {
+                    message
+                }
+            };
+            boundary.Invoke(request);
+            Console.WriteLine("{0} records saved.", _counter);
         }
     }
 }
