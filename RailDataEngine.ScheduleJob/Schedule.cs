e3e3ff05306efd8026760b7f0193bbd26f8c15eb
Tom Lane <tom@tomlane.me>
2015-02-24 20:47:31 +0000

    More cleaning up

--- RailDataEngine.ScheduleJob/Schedule.cs
+++ RailDataEngine.ScheduleJob/Schedule.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.IO;
-using System.IO.Compression;
 using System.Linq;
-using System.Net;
-using System.Text;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Practices.Unity;
 using Microsoft.WindowsAzure.Storage;
@@ -29,9 +24,9 @@ namespace RailDataEngine.ScheduleJob
         {
             _container = ContainerBuilder.Build();
 
-            var scheduleContents = GetScheduleFile();
+            var scheduleContents = ScheduleHelper.GetScheduleFile();
 
-            //SaveToAzureStorage(scheduleContents);
+            SaveToAzureStorage(scheduleContents);
             ApplyScheduleUpdate(scheduleContents);
         }
 
@@ -87,76 +82,9 @@ namespace RailDataEngine.ScheduleJob
             CloudBlobContainer container =
                 blobClient.GetContainerReference(ConfigurationManager.AppSettings["ScheduleContainer"]);
 
-            CloudBlockBlob blockBlob = container.GetBlockBlobReference(GetBlobName());
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(ScheduleHelper.GetBlobName());
             
             blockBlob.UploadText(fileContents);
-        }
-
-        private static string GetBlobName()
-        {
-            var currentDate = DateTime.Now.ToString("ddMMyy");
-
-            return string.Format("fgw-schedule-update-{0}.json", currentDate);
-        }
-        
-        private static string GetScheduleFile()
-        {
-            var request = (HttpWebRequest) WebRequest.Create(GetScheduleUri());
-
-            string authInfo = string.Format("{0}:{1}", ConfigurationManager.AppSettings["Username"],
-                ConfigurationManager.AppSettings["Password"]);
-                
-            authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
-            request.Headers["Authorization"] = "Basic " + authInfo;
-
-            var response = request.GetResponse();
-
-            using (var gzipStream = new GZipStream(response.GetResponseStream(), CompressionMode.Decompress))
-            {
-                const int size = 4096;
-                byte[] buffer = new byte[size];
-                using (MemoryStream memory = new MemoryStream())
-                {
-                    int count = 0;
-                    do
-                    {
-                        count = gzipStream.Read(buffer, 0, size);
-                        if (count > 0)
-                        {
-                            memory.Write(buffer, 0, count);
-                        }
-                    }
-                    while (count > 0);
-                    var array = memory.ToArray();
-
-                    return Encoding.Default.GetString(array);
-                }
-            }
-        }
-
-        private static string GetScheduleUri()
-        {
-            string currentTime = DateTime.Now.DayOfWeek.ToString();
-
-            switch (currentTime)
-            {
-                case "Monday":
-                    return ScheduleUrls.SundaySchedule;
-                case "Tuesday":
-                    return ScheduleUrls.MondaySchedule;
-                case "Wednesday":
-                    return ScheduleUrls.TuesdaySchedule;
-                case "Thursday":
-                    return ScheduleUrls.WednesdaySchedule;
-                case "Friday":
-                    return ScheduleUrls.ThursdaySchedule;
-                case "Saturday":
-                    return ScheduleUrls.FridaySchedule;
-                case "Sunday":
-                    return ScheduleUrls.SaturdaySchedule;
-            }
-
-            return null;
         }
     }
 }
---
+++ RailDataEngine.ScheduleJob/ScheduleHelper.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Text;
+
+namespace RailDataEngine.ScheduleJob
+{
+    internal static class ScheduleHelper
+    {
+        public static string GetBlobName()
+        {
+            var currentDate = DateTime.Now.ToString("ddMMyy");
+
+            return string.Format("fgw-schedule-update-{0}.json", currentDate);
+        }
+
+        public static string GetScheduleFile()
+        {
+            var request = (HttpWebRequest)WebRequest.Create(GetScheduleUri());
+
+            string authInfo = string.Format("{0}:{1}", ConfigurationManager.AppSettings["Username"],
+                ConfigurationManager.AppSettings["Password"]);
+
+            authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
+            request.Headers["Authorization"] = "Basic " + authInfo;
+
+            var response = request.GetResponse();
+
+            using (var gzipStream = new GZipStream(response.GetResponseStream(), CompressionMode.Decompress))
+            {
+                const int size = 4096;
+                byte[] buffer = new byte[size];
+                using (MemoryStream memory = new MemoryStream())
+                {
+                    int count = 0;
+                    do
+                    {
+                        count = gzipStream.Read(buffer, 0, size);
+                        if (count > 0)
+                        {
+                            memory.Write(buffer, 0, count);
+                        }
+                    }
+                    while (count > 0);
+                    var array = memory.ToArray();
+
+                    return Encoding.Default.GetString(array);
+                }
+            }
+        }
+
+        private static string GetScheduleUri()
+        {
+            string currentTime = DateTime.Now.DayOfWeek.ToString();
+
+            switch (currentTime)
+            {
+                case "Monday":
+                    return ScheduleUrls.SundaySchedule;
+                case "Tuesday":
+                    return ScheduleUrls.MondaySchedule;
+                case "Wednesday":
+                    return ScheduleUrls.TuesdaySchedule;
+                case "Thursday":
+                    return ScheduleUrls.WednesdaySchedule;
+                case "Friday":
+                    return ScheduleUrls.ThursdaySchedule;
+                case "Saturday":
+                    return ScheduleUrls.FridaySchedule;
+                case "Sunday":
+                    return ScheduleUrls.SaturdaySchedule;
+            }
+
+            return null;
+        }
+    }
+}
