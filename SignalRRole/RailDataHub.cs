b26b59a613926d75c725069947824f9908c8efbf
Tom Lane <tom@tomlane.me>
2015-03-17 18:53:57 +0000

    Cleaning up some unused/unfinished projects

--- SignalRRole/RailDataHub.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNet.SignalR;
-
-namespace SignalRRole
-{
-    public class RailDataHub : Hub
-    {
-    }
-}
--- deployment.md
+++
@@ -1,1 +0,0 @@
-Testing an automatic deploymeny using azure websites.
